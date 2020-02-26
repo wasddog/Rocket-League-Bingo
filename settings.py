d0965b11dd69eac451cf6a80c3556e93d48afb9f
wasddog <51215035+wasddog@users.noreply.github.com>
2020-02-26 03:35:03 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,52 @@
+challenges = [
+    "Some1 asks for Trade",
+    "Pre-flip goal",
+    "3 Minute overtime",
+    "get into rule no.1",
+    "Team pinch goal",
+    "Score a hat trick",
+    "low five",
+    "passing play goal",
+    "fake an opponent",
+    "get a lag indicator",
+    "opponent has anime PFP",
+    "Someone own-goals",
+    "FREEEE",
+    "make an epic save",
+    "double tap goal",
+    "someone misses open net",
+    "flip reset goal",
+    "ceiling shot",
+    "musty flip goal",
+    "someone in your team whiffs horribly",
+    "toxicity in chat",
+    "opponent using weird car",
+    "opponents rage quit(ff)",
+    "demolish both opponents",
+    "bump/demo goal",
+    "Comeback (3 points or more)",
+    "Last second goal"
+]
+
+
+WIDTH = 600
+HEIGHT = 600
+
+# Colors
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+
+# boards
+testboard = [
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""]
+    ]
+
+# posiotons and sizes
+gridPos = (75,100)
+cellSize = 90
+gridSize = cellSize*5
